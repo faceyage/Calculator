bf10efb9e95443203e0ba33439902148d4a912be
faceyage <alperencingoz33@gmail.com>
2022-06-01 21:51:33 +0300

    Basic structure of page

---
+++ app.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+
+}
+
+function subtract(a, b) {
+
+}
+
+function multiply(a, b) {
+
+}
+
+function divide(a, b) {
+
+}
+
+function operate(operator, a, b) {
+
+}
+
+function display() {
+    
+}
